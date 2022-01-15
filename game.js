a2163c4537c08fd0bb66357ccee6427bb65e20a5
Rio Foster <friotious@gmail.com>
2022-01-15 00:09:39 -0800

    Add player and game class, name functions

--- game.js
+++ game.js
@@ -2,6 +2,32 @@ var player = require('./player');
 
 class Game {
   constructor() {
-    
+    const playerOne = new Player({id: 'PlayerOne', token: 'X' , wins: 0})
+    const playerTwo = new Player({id: 'PlayerTwo', token: 'O', wins: 0})
+    this.playerOneSpacesFilled = []
+    this.playerTwoSpaceFilled = []
+    this.startsNextGame =
+    this.whoseTurn =
+
+  }
+
+  changeTurn() {
+
+  }
+
+  checkForWin() {
+
+  }
+
+  checkForDraw() {
+
+  }
+
+  startsNextGame() {
+
+  }
+
+  resetGameBoard() {
+
   }
 }
--- player.js
+++ player.js
@@ -1,5 +1,12 @@
 class Player {
-  constructor() {
-    
+  constructor(player) {
+    this.id = player.id
+    this.token = player.token;
+    this.wins = 0
+
+  }
+
+  addWin() {
+    this.wins++
   }
 }
