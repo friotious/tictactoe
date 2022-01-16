8584280b69468579179f8fb476b8116f2819d533
Rio Foster <friotious@gmail.com>
2022-01-15 16:31:14 -0800

    Switch spaces filled property from player class to game class

--- game.js
+++ game.js
@@ -4,38 +4,44 @@ class Game {
       id: 'PlayerOne',
       token: 'X',
       wins: 0,
-      spacesFilled: {
-        a: false,
-        b: false,
-        c: false,
-        d: false,
-        e: false,
-        f: false,
-        g: false,
-        h: false,
-        i: false,
-      }
     })
     this.playerTwo = new Player({
       id: 'PlayerTwo',
       token: 'O',
       wins: 0,
-      spacesFilled: {
-        a: false,
-        b: false,
-        c: false,
-        d: false,
-        e: false,
-        f: false,
-        g: false,
-        h: false,
-        i: false,
-      }
     })
+
+    this.playerOneSpacesFilled = {
+      a: false,
+      b: false,
+      c: false,
+      d: false,
+      e: false,
+      f: false,
+      g: false,
+      h: false,
+      i: false,
+    }
+    this.playerTwoSpacesFilled = {
+      a: false,
+      b: false,
+      c: false,
+      d: false,
+      e: false,
+      f: false,
+      g: false,
+      h: false,
+      i: false,
+    }
     this.currentPlayer = this.playerOne;
     this.startsNextGame = this.playerTwo;
 
   }
+
+  checkForWin() {
+
+  }
+
   changeTurn() {
 
   }
