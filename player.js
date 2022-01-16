f4fe20ed49d7db6b1f2c5125419a4baf8c3021ac
Rio Foster <friotious@gmail.com>
2022-01-15 16:16:01 -0800

    Update player properties

--- player.js
+++ player.js
@@ -2,11 +2,13 @@ class Player {
   constructor(player) {
     this.id = player.id
     this.token = player.token;
-    this.wins = 0
+    this.wins = player.wins;
+    this.spacesFilled = player.spaceFilled;
+
 
   }
 
   addWin() {
-    this.wins++
+
   }
 }
