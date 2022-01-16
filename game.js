e47617fb24fe451b888158a0bddfc41ac976b8ea
Rio Foster <friotious@gmail.com>
2022-01-15 18:45:25 -0800

    Add checkForWin method to game class

--- game.js
+++ game.js
@@ -36,15 +36,14 @@ class Game {
     this.currentPlayer = this.playerTwo.token;
     this.startsNextGame = this.playerTwo;
     this.turnCounter = 0;
-    this.pOneWon = false;
-    this.pTwoWon = false;
+    this.weHaveAWinner = false;
   }
 
   changeTurn() {
     if (this.currentPlayer === this.playerOne.token) {
       this.currentPlayer = this.playerTwo.token
     } else {
-      this.currentPlayer = this.playerOne.token)
+      this.currentPlayer = this.playerOne.token
   }
 }
 
@@ -63,16 +62,53 @@ makeAMove(space) {
   }
   addOneTurnCounter()
   changeTurn()
+  checkForWin()
 }
 
 addOneTurnCounter() {
   this.turnCounter++
+  if (this.turnCounter === 9) {
+    checkForDraw()
+  }
 }
 
-checkForWin() {}
+checkForWin() {
+  var win1 = this.playerOneSpacesFilled.a && this.playerOneSpacesFilled.b && this.playerOneSpacesFilled.c;
+  var win2 = this.playerOneSpacesFilled.d && this.playerOneSpacesFilled.e && this.playerOneSpacesFilled.f;
+  var win3 = this.playerOneSpacesFilled.g && this.playerOneSpacesFilled.h && this.playerOneSpacesFilled.i;
+  var win4 = this.playerOneSpacesFilled.a && this.playerOneSpacesFilled.d && this.playerOneSpacesFilled.g;
+  var win5 = this.playerOneSpacesFilled.b && this.playerOneSpacesFilled.e && this.playerOneSpacesFilled.h;
+  var win6 = this.playerOneSpacesFilled.c && this.playerOneSpacesFilled.f && this.playerOneSpacesFilled.i;
+  var win7 = this.playerOneSpacesFilled.a && this.playerOneSpacesFilled.e && this.playerOneSpacesFilled.i;
+  var win8 = this.playerOneSpacesFilled.c && this.playerOneSpacesFilled.e && this.playerOneSpacesFilled.g;
+
+  if (win1 || win2 || win3 || win4 || win5 || win6 || win7 || win8) {
+    this.weHaveAWinner = true;
+    this.playerOne.addWin()
+    changeStartsNextGame()
+    console.log("PLAYER ONE WON");
+  } else {
+    var win1 = this.playerTwoSpacesFilled.a && this.playerTwoSpacesFilled.b && this.playerTwoSpacesFilled.c;
+    var win2 = this.playerTwoSpacesFilled.d && this.playerTwoSpacesFilled.e && this.playerTwoSpacesFilled.f;
+    var win3 = this.playerTwoSpacesFilled.g && this.playerTwoSpacesFilled.h && this.playerTwoSpacesFilled.i;
+    var win4 = this.playerTwoSpacesFilled.a && this.playerTwoSpacesFilled.d && this.playerTwoSpacesFilled.g;
+    var win5 = this.playerTwoSpacesFilled.b && this.playerTwoSpacesFilled.e && this.playerTwoSpacesFilled.h;
+    var win6 = this.playerTwoSpacesFilled.c && this.playerTwoSpacesFilled.f && this.playerTwoSpacesFilled.i;
+    var win7 = this.playerTwoSpacesFilled.a && this.playerTwoSpacesFilled.e && this.playerTwoSpacesFilled.i;
+    var win8 = this.playerTwoSpacesFilled.c && this.playerTwoSpacesFilled.e && this.playerTwoSpacesFilled.g;
+
+    if (win1 || win2 || win3 || win4 || win5 || win6 || win7 || win8) {
+      this.weHaveAWinner = true;
+      this.playerOne.addWin()
+      changeStartsNextGame()
+      console.log("PLAYER TWO WON");
+
+  }
+}
+}
 
 checkForDraw() {
-  if (this.turnCounter === 9 && !this.pOneWon && !this.pTwoWon) {
+  if (!this.weHaveAWinner) {
     console.log('game is a draw')
   }
 }
