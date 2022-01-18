1f2cb932277cc6be58bd3fb80e06c3cc003a13a0
Rio Foster <friotious@gmail.com>
2022-01-17 20:58:19 -0800

    Rename property to match refactored game.js

--- player.js
+++ player.js
@@ -1,14 +1,12 @@
 class Player {
   constructor(player) {
+    this.name = player.name
     this.id = player.id
     this.token = player.token;
     this.wins = player.wins;
-    this.spacesFilled = player.spaceFilled;
-
-
   }
 
   addWin() {
-
+    this.wins++
   }
 }
