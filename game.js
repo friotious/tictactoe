f23525b99ba6c3eb303fb87ab4b9c8a4db869012
Rio Foster <friotious@gmail.com>
2022-01-18 19:23:43 -0800

    Fix multiple errors for draw and win display

--- game.js
+++ game.js
@@ -33,54 +33,53 @@ class Game {
       h: false,
       i: false,
     };
-    this.gameIsADraw = false
-    this.currentPlayer = this.playerOne
-    this.startsNextGame = this.playerTwo
+    this.gameIsADraw = false;
+    this.currentPlayer = this.playerOne;
+    this.startsNextGame = this.playerTwo;
     this.turnCounter = 0;
     this.weHaveAWinner = false;
   }
 
   resetGameboard() {
-    this.changeStartsNextGame()
-    Object.keys(this.playerOneSpacesFilled).forEach(v => this.playerOneSpacesFilled[v] = false);
-    Object.keys(this.playerTwoSpacesFilled).forEach(v => this.playerTwoSpacesFilled[v] = false);
+    this.changeStartsNextGame();
+    Object.keys(this.playerOneSpacesFilled).forEach(
+      (v) => (this.playerOneSpacesFilled[v] = false)
+    );
+    Object.keys(this.playerTwoSpacesFilled).forEach(
+      (v) => (this.playerTwoSpacesFilled[v] = false)
+    );
     this.turnCounter = 0;
-    //this.weHaveAWinner = false;
   }
 
   changeStartsNextGame() {
     if (this.startsNextGame === this.playerTwo) {
-      this.startsNextGame = this.playerOne
+      this.startsNextGame = this.playerOne;
     } else {
-      this.startsNextGame = this.playerTwo
+      this.startsNextGame = this.playerTwo;
     }
   }
 
   checkForDraw() {
     if (!this.weHaveAWinner && this.turnCounter === 9) {
       this.gameIsADraw = true;
-      this.resetGameboard()
-      console.log('game is a draw')
+      displayDraw();
     }
   }
 
   addOneTurnCounter() {
-    this.turnCounter++
-    if (this.turnCounter === 9) {
-      this.checkForDraw()
-    }
+    this.turnCounter++;
   }
 
   changeTurn() {
     if (this.currentPlayer === this.playerOne) {
-      this.currentPlayer = this.playerTwo
+      this.currentPlayer = this.playerTwo;
     } else {
-      this.currentPlayer = this.playerOne
+      this.currentPlayer = this.playerOne;
     }
   }
 
   checkForWin() {
-    var p1 = this.playerOneSpacesFilled
+    var p1 = this.playerOneSpacesFilled;
     var win1 = p1.a && p1.b && p1.c;
     var win2 = p1.d && p1.e && p1.f;
     var win3 = p1.g && p1.h && p1.i;
@@ -92,11 +91,11 @@ class Game {
 
     if (win1 || win2 || win3 || win4 || win5 || win6 || win7 || win8) {
       this.weHaveAWinner = true;
-      this.playerOne.addWin()
-      this.resetGameboard()
-      console.log("PLAYER ONE WON this.startsNextGame should be player2");
+      displayWinner(this.playerOne);
+      this.playerOne.addWin();
+      this.resetGameboard();
     } else {
-      var p2 = this.playerTwoSpacesFilled
+      var p2 = this.playerTwoSpacesFilled;
       var win1 = p2.a && p2.b && p2.c;
       var win2 = p2.d && p2.e && p2.f;
       var win3 = p2.g && p2.h && p2.i;
@@ -108,28 +107,29 @@ class Game {
 
       if (win1 || win2 || win3 || win4 || win5 || win6 || win7 || win8) {
         this.weHaveAWinner = true;
-        this.playerTwo.addWin()
-        this.resetGameboard()
-        console.log("PLAYER TWO WON this.startsNextGame should be player1");
+        displayWinner(this.playerTwo);
+        this.playerTwo.addWin();
+        this.resetGameboard();
       }
     }
   }
 
   makeAMove(space) {
-    if (this.currentPlayer === this.playerOne &&
+    if (
+      this.currentPlayer === this.playerOne &&
       !this.playerOneSpacesFilled[space] &&
       !this.playerTwoSpacesFilled[space]
     ) {
       this.playerOneSpacesFilled[space] = true;
-    } else if (this.currentPlayer === this.playerTwo &&
+    } else if (
+      this.currentPlayer === this.playerTwo &&
       !this.playerTwoSpacesFilled[space] &&
       !this.playerOneSpacesFilled[space]
     ) {
       this.playerTwoSpacesFilled[space] = true;
-    } else {
-      return
     }
-    this.addOneTurnCounter()
-    this.checkForWin()
+    this.addOneTurnCounter();
+    this.checkForDraw();
+    this.checkForWin();
   }
 }
--- main.js
+++ main.js
@@ -1,63 +1,66 @@
+var gameBoard = document.querySelector(".game-board");
+var header = document.querySelector(".player-turn-win-draw");
+var gameSpace = document.querySelectorAll(".game-space");
+var player1NumOfWins = document.querySelector(".player1-number-of-wins");
+var player2NumOfWins = document.querySelector(".player2-number-of-wins");
 
-var gameBoard = document.querySelector('.game-board')
-var header = document.querySelector('.player-turn-win-draw')
-var gameSpace = document.querySelectorAll('.game-space')
-var player1NumOfWins = document.querySelector('.player1-number-of-wins')
-var player2NumOfWins = document.querySelector('.player2-number-of-wins')
+var game = new Game();
+gameBoard.addEventListener("click", function (e) {
+  displayMove(e);
+});
 
-var game = new Game()
-gameBoard.addEventListener('click', function(e){
-  displayMove(e)
-})
-
-displayWins()
+//displayWins()
 
 function resetTimer() {
-  setTimeout(function() {
-    reset()
-    displayWhoseTurn()
+  setTimeout(function () {
+    reset();
+    displayWhoseTurn();
   }, 3000);
 }
 
 function displayWhoseTurn() {
-  header.innerText = `${game.currentPlayer.id}, it's your turn!`
+  header.innerText = `${game.currentPlayer.id}, it's your turn!`;
 }
 
 function displayMove(e) {
-  var letter = document.getElementById(e.target.id)
-  console.log(e.target.id)
-    game.makeAMove(e.target.id)
-    letter.innerHTML = `<img class='player${game.currentPlayer.token}-game-token' src='./assets/${game.currentPlayer.token}.png'/>`
-    displayWhoseTurn()
-    displayWinner()
-    game.checkForDraw()
-    displayDraw()
-    game.changeTurn()
+  var letter = document.getElementById(e.target.id);
+  game.makeAMove(e.target.id);
+  letter.innerHTML = `<img class='player${game.currentPlayer.token}-game-token' src='./assets/${game.currentPlayer.token}.png'/>`;
+  game.checkForWin();
+  if (game.weHaveAWinner) {
+    displayWinner(game.currentPlayer);
+    game.changeTurn();
+  } else if (game.gameIsADraw) {
+    game.changeTurn();
+    displayDraw();
+  } else {
+    game.changeTurn();
+    displayWhoseTurn();
+  }
 }
 
 function reset() {
   for (var i = 0; i < gameSpace.length; i++) {
-    gameSpace[i].innerHTML = ``
+    gameSpace[i].innerHTML = ``;
+    game.gameIsADraw = false;
+    game.weHaveAWinner = false;
+    game.turnCounter = 0;
+    game.resetGameboard();
   }
 }
 
-function displayWinner() {
-  if (game.weHaveAWinner === true) {
-    header.innerText = `${game.currentPlayer.id} Wins!!!`
-    resetTimer()
-    game.weHaveAWinner = false;
-    displayWins()
-  }
+function displayWinner(player) {
+  header.innerText = `${player.id} Wins!!!`;
+  resetTimer();
+  displayWins();
 }
 
 function displayDraw() {
-  if (game.gameIsADraw === true) {
-    game.weHaveAWinner = false;
-    header.innerText = `This Game is a DRAW`
-    resetTimer()
-  }
+  header.innerText = `This Game is a DRAW`;
+  resetTimer();
 }
+
 function displayWins() {
-  player1NumOfWins.innerText = game.playerOne.wins
-  player2NumOfWins.innerText = game.playerTwo.wins
+  player1NumOfWins.innerText = game.playerOne.wins;
+  player2NumOfWins.innerText = game.playerTwo.wins;
 }
